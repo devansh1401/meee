8603e068416290a5b72510c7226f2ffffc97d6b4
Devansh Kukreja <devansh.mmis@gmail.com>
2024-06-28 14:05:39 +0530

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel"; // Import Vercel adapter
+import vercel from '@astrojs/vercel/serverless';
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
