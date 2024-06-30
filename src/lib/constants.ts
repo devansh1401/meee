9f25d45fd2c01af4de8f127b4c6c0c878560f061
Devansh Kukreja <devansh.mmis@gmail.com>
2024-06-30 18:31:18 +0530

    intro card

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,9 @@ export const LINKS = {
   github: "https://github.com/devansh1401",
   linkedin: "https://www.linkedin.com/in/devansh-kukreja/",
   medium: "https://x.com/Devansh_1405",
-  discord: "https://x.com/Devansh_1405",
+  discord: "https://discord.com/users/880029277821763655",
   twitter: "https://x.com/Devansh_1405",
+  Resume: "https://docs.google.com/document/d/1U4cWgiSrSXPIdqooHVxFDwUWewvwnKKdeAuXM6q5wIE/edit?usp=sharing"
 };
 
 export const loaderAnimation = [
